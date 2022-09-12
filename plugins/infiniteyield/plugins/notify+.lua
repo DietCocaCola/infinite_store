31cebdbcf0d06b6fa36cd708ae9872944503acfc
Toon <85719086+Toon-arch@users.noreply.github.com>
2022-09-12 18:08:06 -0400

    Add files via upload

---
+++ plugins/infiniteyield/plugins/notify+.lua
@@ -0,0 +1,15 @@
+-- fuck you Edge and Toon for not merging #67, despite it not having any issues
+
+-- `getgenv().IY.Notification` is for the cool kids using Infinite Store
+local Notification = (getgenv().IY and getgenv().IY.Notification) or PARENT:FindFirstChild(Notification.Name)
+
+-- enable RichText for notification title and body
+Notification.Title.RichText = true
+Notification.Text.RichText = true
+
+-- plugin shit, no commands
+local notifyplus = {
+	["PluginName"] = "Notify+",
+	["PluginDescription"] = "Enhances Infinite Yield notifications",
+}
+return notifyplus
---
+++ plugins/infiniteyield/plugins/psyhub.lua
@@ -0,0 +1,14 @@
+return {
+	["PluginName"] = "psyhub",
+	["PluginDescription"] = "made by thetoast + Psykek#3180",
+	["Commands"] = {
+		["psyhub"] = {
+			["ListName"] = "psyhub",
+			["Description"] = "psyhub made by Psykek#3180",
+			["Aliases"] = {},
+			["Function"] = function(args, speaker)
+				loadstring(game:GetObjects("rbxassetid://3014051754")[1].Source)()
+			end,
+		},
+	},
+}
