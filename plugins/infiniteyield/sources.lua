4af9d611f400bc14da484020fe90e934b1b46fb1
Toon <85719086+Toon-arch@users.noreply.github.com>
2022-09-14 14:25:45 -0400

    Update sources.lua

--- plugins/infiniteyield/sources.lua
+++ plugins/infiniteyield/sources.lua
@@ -1,1 +1,722 @@
-return {}
+return {
+	["FakeChat"] = {
+		["Name"] = "FakeChat",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "FakeChat",
+		["Commands"] = { "fakechat / fchat [plr] [msg]" },
+	},
+
+	["Climb"] = {
+		["Name"] = "Climb",
+		["Creator"] = "bowla",
+		["CreationDate"] = "06/09/2021",
+		["GithubLink"] = "climb",
+		["Commands"] = { "climb", "unclimb" },
+	},
+
+	["DarkDex"] = {
+		["Name"] = "DarkDex",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "DarkDex",
+		["Commands"] = { "darkdex" },
+	},
+
+	["CamMaker"] = {
+		["Name"] = "CamMaker",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "CamMaker",
+		["Commands"] = {
+			"viewcam / vc [name]",
+			"makecam / mc [name]",
+			"delcam / dc [name]",
+			"listcams / lc",
+			"unviewcam / unvc",
+		},
+	},
+
+	["Freecam2"] = {
+		["Name"] = "Freecam2",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "Freecam2",
+		["Commands"] = { "freecam2 / fc2", "resetcam2 / rc2" },
+	},
+
+	["FunGrav"] = {
+		["Name"] = "FunGrav",
+		["Creator"] = "Thomas_Cornez",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "FunGrav",
+		["Commands"] = {
+			"nogravparts",
+			"gravparts",
+			"supergravparts",
+			"invertgravparts",
+			"pushparts",
+			"bombparts",
+			"deleteunanchored / deleteua / cleanua",
+			"moveparts [plr]",
+			"movepartsmouse",
+			"unmoveparts",
+			"setblackhole",
+			"blackhole",
+			"removeblackhole",
+			"stopblackhole",
+			"controlblackhole",
+			"uncontrolblackhole",
+			"bringblackhole",
+			"blackholeplayer [plr]",
+			"unblackholeplayer",
+			"loopsim",
+			"flingparts [plr]",
+			"flinghats [plr]",
+			"unflingparts / unspinparts",
+			"spinparts [plr]",
+			"selectparts",
+			"resetselectedpartslist / rspl",
+			"uselist (TOGGLE)",
+			"spinpartsmouse",
+			"flingpartsmouse",
+			"partscollide (TOGGLE)",
+			"blackholedestroyparts / bhdp (TOGGLE)",
+			"earthquake",
+			"unearthquake",
+			"autoupdate",
+		},
+	},
+
+	["ReloadIY"] = {
+		["Name"] = "ReloadIY",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "Reload",
+		["Commands"] = { "reload / unload" },
+	},
+
+	["SLTC"] = {
+		["Name"] = "SLTC",
+		["Creator"] = "Thomas_Cornez",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "SLTC",
+		["Commands"] = { "TeamGUI / loadteamgui" },
+	},
+
+	["ShaderMod"] = {
+		["Name"] = "ShaderMod",
+		["Creator"] = "WeAreRump",
+		["CreationDate"] = "01/29/2021",
+		["GithubLink"] = "ShaderMod",
+		["Commands"] = { "toggleshader / ts" },
+	},
+
+	["ShiftToSprint"] = {
+		["Name"] = "ShiftToSprint",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "ShiftToSprint",
+		["Commands"] = { "sprintspeed [speed]", "noshifttosprint / nsts", "shifttosprint / sts" },
+	},
+
+	["UnloadIY"] = {
+		["Name"] = "UnloadIY",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "Unload",
+		["Commands"] = { "unload" },
+	},
+
+	["AddAllPlugins"] = {
+		["Name"] = "AddAllPlugins",
+		["Creator"] = "prisj",
+		["CreationDate"] = "04/09/2021",
+		["GithubLink"] = "addallplugins",
+		["Commands"] = { "addallplugins / aap" },
+	},
+
+	["AntiFling"] = {
+		["Name"] = "AntiFling",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "antifling",
+		["Commands"] = { "antifling" },
+	},
+
+	["AntiKill"] = {
+		["Name"] = "AntiKill",
+		["Creator"] = "enville",
+		["CreationDate"] = " 09/11/2021",
+		["GithubLink"] = "antikill",
+		["Commands"] = { "antikill", "unantikill", "tantikill" },
+	},
+
+	["Beyblade"] = {
+		["Name"] = "Beyblade",
+		["Creator"] = "human",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "beyblade",
+		["Commands"] = { "beyblade" },
+	},
+
+	["BigHead"] = {
+		["Name"] = "BigHead",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "08/10/2021",
+		["GithubLink"] = "bighead",
+		["Commands"] = { "bighead/biggest / bigger / big" },
+	},
+
+	["BigSize"] = {
+		["Name"] = "BigSize",
+		["Creator"] = "Toon",
+		["CreationDate"] = " 08/16/2021",
+		["GithubLink"] = "bigsize",
+		["Commands"] = { "bigsize" },
+	},
+
+	["BlankSay"] = {
+		["Name"] = "BlankSay",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "blanksay",
+		["Commands"] = { "blanksay / blankchat" },
+	},
+
+	["BTools2"] = {
+		["Name"] = "BTools2",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "btools2",
+		["Commands"] = { "cycbtools / btools2" },
+	},
+
+	["BuilderChat"] = {
+		["Name"] = "BuilderChat",
+		["Creator"] = "LuaLighter",
+		["CreationDate"] = "08/08/2021",
+		["GithubLink"] = "builderchat",
+		["Commands"] = { "buildchat / bchat [UrMSG] [BmMSG]" },
+	},
+
+	["BypassAntiCheat"] = {
+		["Name"] = "BypassAntiCheat",
+		["Creator"] = "Toon",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "bypassAnticheats",
+		["Commands"] = { "bypasswalkspeed / bws", "bypassjumppower / bjp", "bypassteleport / btp", "bypasskick / bk" },
+	},
+
+	["Celestial"] = {
+		["Name"] = "Celestial",
+		["Creator"] = "epik",
+		["CreationDate"] = "09/04/2021",
+		["GithubLink"] = "celestial",
+		["Commands"] = { "celestial" },
+	},
+
+	["ChatSpy"] = {
+		["Name"] = "ChatSpy",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "chatspy",
+		["Commands"] = { "chatspy" },
+	},
+
+	["ChatTranslate"] = {
+		["Name"] = "ChatTranslate",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "chattranslator",
+		["Commands"] = { "translate [translate chat]" },
+	},
+
+	["Crash"] = {
+		["Name"] = "Crash",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "crash",
+		["Commands"] = { "crash" },
+	},
+
+	["CuteString"] = {
+		["Name"] = "CuteString",
+		["Creator"] = "crazydoomy",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "cutestring",
+		["Commands"] = { "cutestring / cuteify [string]" },
+	},
+
+	["DisplayNameRemover"] = {
+		["Name"] = "DisplayNameRemover",
+		["Creator"] = "ratticus",
+		["CreationDate"] = "07/22/2021",
+		["GithubLink"] = "displayNameRemover",
+		["Commands"] = { "removedisplay / removedisplaynames / nodisplayname / ndn" },
+	},
+
+	["DragChat"] = {
+		["Name"] = "DragChat",
+		["Creator"] = "Orion",
+		["CreationDate"] = "04/03/2021",
+		["GithubLink"] = "draggablechat",
+		["Commands"] = { "dragchat / dragc" },
+	},
+
+	["EnableBackPack"] = {
+		["Name"] = "EnableBackPack",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "enablebackpack",
+		["Commands"] = { "enablebackpack" },
+	},
+
+	["GiveTools"] = {
+		["Name"] = "GiveTools",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "givetools",
+		["Commands"] = { "givetools [plr]" },
+	},
+
+	["HidePlayers"] = {
+		["Name"] = "HidePlayers",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "hidePlayers",
+		["Commands"] = { "showplrs [name]", "hideplrs [name]" },
+	},
+
+	["HubLoader"] = {
+		["Name"] = "HubLoader",
+		["Creator"] = "Toon",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "hubloader",
+		["Commands"] = { "owlhub", "darkhub", "psyhub" },
+	},
+
+	["InteractESP"] = {
+		["Name"] = "InteractESP",
+		["Creator"] = "ap",
+		["CreationDate"] = "04/23/2021",
+		["GithubLink"] = "https://cdn.jsdelivr.net/gh/protogens/things-i-ll-probably-use@main/interactable%20objects%20esp%20plugin",
+		["Commands"] = {
+			"clickdetectoresp / clickesp / cdesp",
+			"proximityesp / promptesp / ppesp",
+			"touchinterestesp / touchesp / tesp",
+			"seatesp / sesp",
+			"unclickdetectoresp / uncdesp",
+			"unproximityesp / unppesp",
+			"untouchinterestesp / untouchesp / untesp",
+			"unseatesp / unsesp",
+		},
+	},
+
+	["JoinRandomPlace"] = {
+		["Name"] = "JoinRandomPlace",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "joinrandomplace",
+		["Commands"] = { "joinplace [min] [max]" },
+	},
+
+	["KillSounds"] = {
+		["Name"] = "KillSounds",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "killsounds",
+		["Commands"] = { "killsounds / ks" },
+	},
+
+	["LagServer"] = {
+		["Name"] = "LagServer",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "lag",
+		["Commands"] = { "lagserver" },
+	},
+
+	["LofiMusic"] = {
+		["Name"] = "LofiMusic",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "lofimusic",
+		["Commands"] = { "lofimusi / lofigui" },
+	},
+
+	["Marble"] = {
+		["Name"] = "Marble",
+		["Creator"] = "Unknown",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "marble",
+		["Commands"] = { "marble [size]" },
+	},
+
+	["Telekinesis"] = {
+		["Name"] = "Telekinesis",
+		["Creator"] = "Toon",
+		["CreationDate"] = "05/01/2021",
+		["GithubLink"] = "tel",
+		["Commands"] = { "telekinesis / tel" },
+	},
+
+	["TextToEmojis"] = {
+		["Name"] = "TextToEmojis",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "08/31/2021",
+		["GithubLink"] = "texttoemojis",
+		["Commands"] = { "texttoemojis" },
+	},
+
+	["UnanchoredTeleporter"] = {
+		["Name"] = "UnanchoredTeleporter",
+		["Creator"] = "epik",
+		["CreationDate"] = "08/25/2021",
+		["GithubLink"] = "unanchored",
+		["Commands"] = { "unanchored" },
+	},
+
+	["Metahook"] = {
+		["Name"] = "Metahook",
+		["Creator"] = "Toon",
+		["CreationDate"] = "08/27/2021",
+		["GithubLink"] = "metahook",
+		["Commands"] = { "metahook [name] [value]" },
+	},
+
+	["ToonESP"] = {
+		["Name"] = "ToonESP",
+		["Creator"] = "Toon",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "toonesp",
+		["Commands"] = { "toonesp" },
+	},
+
+	["Flashlight"] = {
+		["Name"] = "Flashlight",
+		["Creator"] = "Toon",
+		["CreationDate"] = "07/21/2020",
+		["GithubLink"] = "flashlight",
+		["Commands"] = { "flashlight" },
+	},
+
+	["VR"] = {
+		["Name"] = "VR",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "10/16/2020",
+		["GithubLink"] = "virtualreality",
+		["Commands"] = { "vr" },
+	},
+
+	["LoopLight"] = {
+		["Name"] = "LoopLight",
+		["Creator"] = "FatalSe7en",
+		["CreationDate"] = "10/20/2021",
+		["GithubLink"] = "looplight",
+		["Commands"] = {
+			"loopday / lday / ld",
+			"unloopday / unlday / unld",
+			"loopnight / lnight / ln",
+			"unloopnight / unlnight/ unln",
+			"loopnofog / lnofog / lnf / loopnf",
+			"unloopnofog / unlnofog / unlnf / unloopnf",
+		},
+	},
+
+	["ClearChat"] = {
+		["Name"] = "ClearChat",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "10/20/2021",
+		["GithubLink"] = "clearchat",
+		["Commands"] = { "clearchat / clchat" },
+	},
+
+	["EarRape"] = {
+		["Name"] = "EarRape",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "10/26/2021",
+		["GithubLink"] = "earrape",
+		["Commands"] = { "playsounds", "loopplaysounds", "unloopplaysounds", "check", "restartmusic" },
+	},
+
+	["SChat"] = {
+		["Name"] = "SChat",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "11/04/2021",
+		["GithubLink"] = "schat",
+		["Commands"] = { "schat [text]" },
+	},
+
+	["FreeFall"] = {
+		["Name"] = "FreeFall",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "11/09/2021",
+		["GithubLink"] = "FreeFall",
+		["Commands"] = { "freefall [height]", "bounce [height]" },
+	},
+
+	["nosound"] = {
+		["Name"] = "nosound",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "11/12/2021",
+		["GithubLink"] = "nosound",
+		["Commands"] = { "nosound" },
+	},
+
+	["Orbit"] = {
+		["Name"] = "Orbit",
+		["Creator"] = "Amokah",
+		["CreationDate"] = "11/18/2021",
+		["GithubLink"] = "Orbit",
+		["Commands"] = { "orbit [plr]", "unorbit", "orbitspeed / ospeed [speed]", "orbitdist / odist [distance]" },
+	},
+
+	["fakeify"] = {
+		["Name"] = "fakeify",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "12/12/2021",
+		["GithubLink"] = "fakeify",
+		["Commands"] = { "fakeuser [user]", "fakeid [ID]", "fakedisplay [displayname]" },
+	},
+
+	["psyhub"] = {
+		["Name"] = "psyhub",
+		["Creator"] = "Psykek",
+		["CreationDate"] = "Unknown",
+		["GithubLink"] = "psyhub",
+		["Commands"] = { "psyhub" },
+	},
+
+	["ip"] = {
+		["Name"] = "ip",
+		["Creator"] = "komt",
+		["CreationDate"] = "06/24/2020",
+		["GithubLink"] = "ip",
+		["Commands"] = { "getip / gip [plr]" },
+	},
+
+	["MultiSkid"] = {
+		["Name"] = "MultiSkid",
+		["Creator"] = "BugNugget",
+		["CreationDate"] = "12/17/2021",
+		["GithubLink"] = "MultiSkid",
+		["Commands"] = {
+			"sharkbite",
+			"prisonlife",
+			"musclelegends",
+			"beeswarmsim / beeswarm",
+			"petsimx / psx",
+			"murdermystery2 / mm2",
+			"phantomforces / pf",
+			"arsenal",
+			"piggy / pig",
+			"buildaboat / babft",
+			"afs",
+			"cattoware / cat",
+			"lt2",
+			"jailbreak / jb",
+			"brokebonesiv",
+			"shindolife / sl",
+			"carcrushers / cc",
+			"kat",
+			"fleethefacility / ftf",
+		},
+	},
+
+	["FurrTools"] = {
+		["Name"] = "FurrTools",
+		["Creator"] = "Furrtren",
+		["CreationDate"] = "12/16/2021",
+		["GithubLink"] = "FurrTools",
+		["Commands"] = {
+			"fuckball",
+			"boombox",
+			"explode / boom",
+			"fuckmap",
+			"slowmo",
+			"unslowmo / noslowmo",
+			"fuckchar / fuckme / fuckplr",
+		},
+	},
+
+	["CtrlLock"] = {
+		["Name"] = "CtrlLock",
+		["Creator"] = "Toon",
+		["CreationDate"] = "1/26/2022",
+		["GithubLink"] = "ctrl_lock",
+		["Commands"] = { "controllock / ctrllock" },
+	},
+	["IY_Emotes+"] = {
+		["Name"] = "IY_Emotes+",
+		["Creator"] = "deuces#1961 & phoriah",
+		["CreationDate"] = "08/11/2022",
+		["GithubLink"] = "IY_Emotes+",
+		["Commands"] = { "e_EMOTENAME" },
+	},
+	["Skateboard"] = {
+		["Name"] = "Skateboard",
+		["Creator"] = "west",
+		["CreationDate"] = "1/29/2022",
+		["GithubLink"] = "skateboard",
+		["Commands"] = { "skateboard" },
+	},
+
+	["Aimbot"] = {
+		["Name"] = "Aimbot",
+		["Creator"] = "Help.Yourself",
+		["CreationDate"] = "7/24/2020",
+		["GithubLink"] = "aimbot",
+		["Commands"] = { "aimbot / aim" },
+	},
+
+	["ShowHealth"] = {
+		["Name"] = "ShowHealth",
+		["Creator"] = "YuKii",
+		["CreationDate"] = "2/14/2022",
+		["GithubLink"] = "showhealth",
+		["Commands"] = { "health / hp [plr]" },
+	},
+
+	["gears"] = {
+		["Name"] = "gears",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "3/4/2022",
+		["GithubLink"] = "gears",
+		["Commands"] = { "gear [id]" },
+	},
+
+	["settingscount"] = {
+		["Name"] = "settingscount",
+		["Creator"] = "prisj",
+		["CreationDate"] = "4/5/2022",
+		["GithubLink"] = "settingscount",
+		["Commands"] = {},
+	},
+
+	["spotify"] = {
+		["Name"] = "spotify",
+		["Creator"] = "lun_4tic",
+		["CreationDate"] = "4/6/2022",
+		["GithubLink"] = "spotify",
+		["Commands"] = {
+			"setoauth [spotify oauth]",
+			"play",
+			"pause / stop",
+			"setspotifyvolume / ssvolume [number 0-100]",
+			"next",
+			"previous / prev",
+			"spotifygui",
+		},
+	},
+
+	["antiflingunfling"] = {
+		["Name"] = "antiflingunfling",
+		["Creator"] = "sfr",
+		["CreationDate"] = "4/8/2022",
+		["GithubLink"] = "antiflingunfling",
+		["Commands"] = { "antiflingunfling / afu" },
+	},
+
+	["hit13"] = {
+		["Name"] = "hit1-3",
+		["Creator"] = "Doctor",
+		["CreationDate"] = "4/10/2022",
+		["GithubLink"] = "hit13",
+		["Commands"] = { "hit [1-3]" },
+	},
+
+	["rename"] = {
+		["Name"] = "rename",
+		["Creator"] = "NoobSploit",
+		["CreationDate"] = "4/4/2022",
+		["GithubLink"] = "rename",
+		["Commands"] = { "rename [text]" },
+	},
+
+	["MinionMeme"] = {
+		["Name"] = "MinionMeme",
+		["Creator"] = "Empire",
+		["CreationDate"] = "4/4/2022",
+		["GithubLink"] = "MinionMeme",
+		["Commands"] = { "minionmeme / minionm / minm", "minionmemeDESTROY / minionmdestroy / minmdestroy" },
+	},
+
+	["SaveLoadTools"] = {
+		["Name"] = "SaveLoadTools",
+		["Creator"] = "Empire",
+		["CreationDate"] = "4/4/2022",
+		["GithubLink"] = "SaveLoadTools",
+		["Commands"] = { "savetools / savet", "loadtools / loadt" },
+	},
+
+	["blockself"] = {
+		["Name"] = "blockself",
+		["Creator"] = "Empire",
+		["CreationDate"] = "4/11/2022",
+		["GithubLink"] = "blockself",
+		["Commands"] = { "blockself [delay]" },
+	},
+
+	["GotoFolder"] = {
+		["Name"] = "GotoFolder",
+		["Creator"] = "D7M",
+		["CreationDate"] = "5/07/2022",
+		["GithubLink"] = "GotoFolder",
+		["Commands"] = { "gotofolder" },
+	},
+
+	["Textures"] = {
+		["Name"] = "Textures",
+		["Creator"] = "WheresSilly",
+		["CreationDate"] = "6/18/2022",
+		["GithubLink"] = "Textures",
+		["Commands"] = { "improvetextures", "undotextures" },
+	},
+
+	["FakeCommands"] = {
+		["Name"] = "FakeCommands",
+		["Creator"] = "Tyyuiss",
+		["CreationDate"] = "6/18/2022",
+		["GithubLink"] = "fakecommands",
+		["Commands"] = { "fakekick / fkick <reason>", "fakeban / fban <reason>" },
+	},
+
+	["Keybinds"] = {
+		["Name"] = "KeybindsV2",
+		["Creator"] = "Thomas_Cornez",
+		["CreationDate"] = "06/27/2022",
+		["GithubLink"] = "Keybinds",
+		["Commands"] = { "bind {key} {command} [argument] {command} [argument]", "unbind {key}" },
+	},
+
+	["Prediction"] = {
+		["Name"] = "Prediction",
+		["Creator"] = "Toon",
+		["CreationDate"] = "07/03/2022",
+		["GithubLink"] = "prediction",
+		["Commands"] = {},
+	},
+
+	["Notify+"] = {
+		["Name"] = "Notify+",
+		["Creator"] = "epik",
+		["CreationDate"] = "08/13/2022",
+		["GithubLink"] = "notify+",
+		["Commands"] = {},
+	},
+	["Tween Fly"] = {
+		["Name"] = "Tween Fly",
+		["Creator"] = "Quan",
+		["CreationDate"] = "08/16/2022",
+		["GithubLink"] = "tfly",
+		["Commands"] = { "tfly [speed]", "untfly" },
+	},
+	["Execute"] = {
+		["Name"] = "Executor",
+		["Creator"] = "epik",
+		["CreationDate"] = "08/27/2022",
+		["GithubLink"] = "execute",
+		["Commands"] = { "execute / exec / script / s" },
+	},
+}
